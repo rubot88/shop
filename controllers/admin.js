2282e4858d09ef8963130821fee8b55cf6853320
Rustam Botirov <rustam.botirov@923digital.com>
2020-04-25 20:17:17 +0300

    7843216 Pre-populating the edit page with data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,38 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: "Add Product",
-    path: "/admin/add-product"
+    path: "/admin/add-product",
+    editing:false
   })
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const {title, imageUrl, price, description} = req.body;
+  const { title, imageUrl, price, description } = req.body;
   const product = new Product(title, imageUrl, price, description);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editing = req.query.edit;
+  if (!editing) {
+    return res.redirect('/');
+  }
+  const productId = req.params.productId;
+  Product.findById(productId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing,
+      product,
+    })
+  })
+
 };
 
 

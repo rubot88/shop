e57a927375a789044b91608e55cacf28f2670e49
Rustam Botirov <rustam.botirov@923digital.com>
2020-04-25 21:00:00 +0300

    78431654

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,13 +4,13 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    editing:false
+    editing: false
   })
 };
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, price, description } = req.body;
-  const product = new Product(title, imageUrl, price, description);
+  const product = new Product(null, title, imageUrl, price, description);
   product.save();
   res.redirect('/');
 };
@@ -35,6 +35,13 @@ exports.getEditProduct = (req, res, next) => {
 
 };
 
+
+exports.postEditProduct = (req, res, next) => {
+  const { id, title, imageUrl, price, description } = req.body;
+  const updatedProduct = new Product(id, title, imageUrl, price, description);
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {

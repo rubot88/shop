29a963f59ced0ba57fda10852724ccecd447ea58
Rustam Botirov <rustam.botirov@923digital.com>
2020-04-26 20:02:54 +0300

    1321654

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,19 +33,39 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    pageTitle: 'Your Cart',
-    path: "/cart",
-  });
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      products.forEach(product => {
+        const cartProduct = cart.products.find(p => product.id === p.id);
+        if (cartProduct) {
+          cartProducts.push({ product, qty: cartProduct.qty });
+        }
+      });
+      res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: "/cart",
+        products: cartProducts,
+      });
+    });
+  })
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, product => {
-    Cart.addProduct(prodId,product.price);
+    Cart.addProduct(prodId, product.price);
   })
 
   res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('cart');
+  })
 };
 
 exports.getCheckout = (req, res, next) => {

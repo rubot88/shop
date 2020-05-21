6fd39d68de83ab5d1032d844eb4bd8d15f54ae4f
Rustam Botirov <rustam.botirov@923digital.com>
2020-05-21 09:04:22 +0300

    456314654 Add items to cart; Display cart

--- middlewares/setUser.js
+++ middlewares/setUser.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 const setUser = async (req, res, next) => {
     try {
-        const { username, email, cart, _id } = await User.findById('5ec4ba4c53b435267ccd8ea2');
+        const { username, email, cart, _id } = await User.findById('5ec60ca6f32605c652e401e3');
         // await user.createCart();
         req.user = new User(username, email, cart, _id);
         next();
